f8186d011c690e00e1aaea06c009b84030140f23
Oleg <potrebchuk0@gmail.com>
2026-06-22 11:44:00 +0300

    added logging middleware

---
+++ internal/core/middleware/logging.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func LoggingMiddleware(log *zap.Logger, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+
+		next.ServeHTTP(rw, r)
+
+		log.Info("request",
+			zap.String("method", r.Method),
+			zap.String("path", r.URL.Path),
+			zap.Int("status", rw.statusCode),
+			zap.Duration("duration", time.Since(start)),
+		)
+	})
+}
